ab2d1b21165b02c62560ef58cd853e3d3d7a3d92
RichardHpa <richard.hpa@hotmail.com>
2021-11-23 22:35:45 +1300

    added  basic auth

--- server.ts
+++ server.ts
@@ -1,7 +1,10 @@
+require('dotenv').config();
 import express from 'express';
 import cors from 'cors';
 import type { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
+import nodeMailer from 'nodemailer';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 // Set up middleware
@@ -81,6 +84,41 @@ app.post('/register', async (req: Request, res: Response) => {
   }
 });
 
+// Set up email
+// const transport = nodeMailer.createTransport({
+//   host: process.env.EMAIL_HOST,
+//   port: 587,
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASSWORD,
+//   },
+// });
+const transporter = nodeMailer.createTransport({
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+    user: 'jocelyn.schuppe54@ethereal.email',
+    pass: 'kvBb923R6X1KEyEBxG',
+  },
+});
+
+interface Email {
+  name: string;
+  link: string;
+}
+
+const emailTemplate = ({ name, link }: Email) => `
+  <h2>Hey ${name}</h2>
+  <p>Here's the login link you just requested:</p>
+  <p>${link}</p>
+`;
+
+// Generate token
+const makeToken = (email: string) => {
+  const expirationDate = new Date();
+  expirationDate.setHours(new Date().getHours() + 1);
+  return jwt.sign({ email, expirationDate }, process.env.JWT_SECRET_KEY as string);
+};
 // login
 app.post('/login', (req: Request, res: Response) => {
   const email = req.body.email;
@@ -93,8 +131,25 @@ app.post('/login', (req: Request, res: Response) => {
   }
 
   if (email) {
-    res.status(200);
-    res.send(email);
+    const token = makeToken(email);
+    const mailOptions = {
+      from: 'You Know',
+      html: emailTemplate({
+        name: email,
+        link: `http://localhost:3000/account?token=${token}`,
+      }),
+      subject: 'Your Magic Link',
+      to: email,
+    };
+    return transporter.sendMail(mailOptions, error => {
+      if (error) {
+        res.status(404);
+        res.send("Can't send email.");
+      } else {
+        res.status(200);
+        res.send(`Magic link sent. : http://localhost:3000/account?token=${token}`);
+      }
+    });
   }
 });
 
@@ -107,6 +162,40 @@ app.get('/users/:id', async (req: Request, res: Response) => {
   const { id } = req.params;
   const user = await prisma.user.findFirst({ where: { id } });
   res.json(user);
+});
+
+const isAuthenticated = (req: any, res: any) => {
+  const { token } = req.query;
+  if (!token) {
+    res.status(403);
+    res.send("Can't verify user.");
+    return;
+  }
+  let decoded;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
+  } catch {
+    res.status(403);
+    res.send('Invalid auth credentials.');
+    return;
+  }
+  if (!decoded.hasOwnProperty('email') || !decoded.hasOwnProperty('expirationDate')) {
+    res.status(403);
+    res.send('Invalid auth credentials.');
+    return;
+  }
+  const { expirationDate }: any = decoded;
+  if (expirationDate < new Date()) {
+    res.status(403);
+    res.send('Token has expired.');
+    return;
+  }
+  res.status(200);
+  res.send('User has been validated.');
+};
+// Get account information
+app.get('/account', (req, res) => {
+  isAuthenticated(req, res);
 });
 
 app.listen(3000, () => {
